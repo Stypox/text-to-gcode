e7393c898a3ac7429b667b23afd9091672894009
Stypox <stypox@pm.me>
2019-01-12 16:14:03 +0100

    Added interface to parse Letters.

---
+++ text-to-gcode.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+
+from enum import Enum
+import os
+
+class Instr:
+	class Type(Enum):
+		move = 0,
+		write = 1,
+
+	def __init__(self, *args):
+		if len(args) == 1 and type(args[0]) is str: # args must be a data str
+			attributes = args[0].split(' ')
+			# G_ X__ Y__
+			self.type = Instr.Type.move if attributes[0][1] == '0' else Instr.Type.write
+			self.x = float(attributes[1][1:])
+			self.y = float(attributes[2][1:])
+		elif len(args) == 3 and type(args[0]) is Instr.Type and type(args[1]) is float and type(args[2]) is float:
+			self.type, self.x, self.y = args
+		else:
+			raise TypeError("Instr() takes one (str) or three (Instr.Type, float, float) arguments")
+	
+	def __repr__(self):
+		return "G%d X%.2f Y%.2f" % (self.type.value[0], self.x, self.y)
+
+	def translated(self, x, y):
+		return Instr(self.type, self.x + x, self.y + y)
+
+class Letter:
+	def __init__(self, *args):
+		if len(args) == 1 and type(args[0]) is str:
+			self.instructions = []
+			for line in args[0].split('\n'):
+				if line != "":
+					self.instructions.append(Instr(line))
+		elif len(args) == 1 and type(args[0]) is list:
+			self.instructions = args[0]
+		else:
+			raise TypeError("Letter() takes one (str) or one (list) arguments")		
+	
+	def __repr__(self):
+		return "\n".join([repr(instr) for instr in self.instructions])
+	
+	def translated(self, x, y):
+		return Letter([instr.translated(x, y) for instr in self.instructions])
+
+def readLetters(directory):
+	letters = {}
+	for root,_,filenames in os.walk(directory):
+		for filename in filenames:
+			file = open(os.path.join(root,filename),"r")
+			letterRepr = file.readline()[1]
+			letter = Letter(file.read())
+			letters[letterRepr] = letter
+	return letters
+
+def main(argv):
+	letters = readLetters("./ascii_gcode/")
+	print(*[k for k,v in letters.items()])
+	print(letters["A"].translated(1, 1))
+
+if __name__ == '__main__':
+	from sys import argv
+	main(argv)
